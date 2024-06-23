eb5e0dc246c7142a3b07263447e6c2f2487f1769
z4hid <zahidhasan9297@gmail.com>
2024-06-23 21:11:55 +0600

    setup file created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'DrishtiDrive',
+    version= '0.0.1',
+    author= 'Zahid Hasan',
+    author_email= 'zahidhasan9297@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
